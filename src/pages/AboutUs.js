f5fe9f3e12f870e556da13b3176a2438ddb6c341
Joel Kuruvilla <jl567056@dal.ca>
2024-05-01 16:51:50 -0300

    Created some pages and reorganized image files

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,11 @@
+/*
+About Us Page
+Created: 2024-APR-29
+*/
+export default function AboutUs() {
+    return (
+        <div>
+            <h1> Hello There! </h1>
+        </div>
+    );
+}
---
+++ src/pages/Login.js
@@ -0,0 +1,5 @@
+/*
+About Us Page
+Created: 2024-APR-29
+*/
+//TBD.
